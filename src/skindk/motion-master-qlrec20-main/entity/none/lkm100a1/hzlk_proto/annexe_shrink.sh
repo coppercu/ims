0f00fd72a3ee079c05274dee8617e4a64a2b218a
coppercu <coppercu@qq.com>
2022-09-26 00:15:57 +0800

    skeleton

--- src/skindk/motion-master-qlrec20-main/entity/none/lkm100a1/hzlk_proto/annexe_shrink.sh
+++ src/skindk/motion-master-qlrec20-main/entity/none/lkm100a1/hzlk_proto/annexe_shrink.sh
@@ -3,8 +3,30 @@
 #
 #
 
-if [ ! -f ../../../../../../../imk/src/scripts/entity/shrink.sh ]; then
+pushd ../../../../../../../ > /dev/null
+
+imx_path=`pwd`
+
+if [ -f imk.path ]; then
+    imk_path_cite=`cat imk.path`
+else
+    imk_path_cite=imk
+fi
+if [ ! -d $imk_path_cite ]; then
+    read -p "imk路径未找到，请输入任意键退出" key
+else
+    pushd $imk_path_cite > /dev/null
+    imk_path=`pwd`
+    popd > /dev/null
+fi
+
+popd > /dev/null
+
+pwd_path=`pwd`
+rel_path=${pwd_path##${imx_path}/}
+
+if [ ! -f $imk_path/src/scripts/entity/shrink.sh ]; then
     read -p "shrink.sh未找到，确认imk工程是否加载" key
     exit
 fi
-source ../../../../../../../imk/src/scripts/entity/shrink.sh $PWD
+source $imk_path/src/scripts/entity/shrink.sh
